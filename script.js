2c2f96d8a4b9967b4eb1601b81a05f143047d9c7
Michael Banzeu <banzeumike@gmail.com>
2023-08-04 21:26:55 +0100

    Dynamic date added (can be define by the user)

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-setInterval(() => {
+const selectedDate = document.getElementById('date');
+
+window.addEventListener('load', () => {
+    selectedDate.value = "2025-01-01";
+})
+
+function countDown() {
     let now = new Date();
-    let eventDate = new Date(2100, 1, 1);
+    let eventDate = new Date(selectedDate.value);
+    let options = { year: 'numeric', month: 'long', day: 'numeric' };
     let currentTime = now.getTime();
     let eventTime = eventDate.getTime();
     let remTime = eventTime - currentTime;
@@ -8,17 +15,16 @@ setInterval(() => {
     let m = Math.floor(s / 60);
     let h = Math.floor(m / 60);
     let d = Math.floor(h / 24);
-
+    
     h %= 24;
     m %= 60;
     s %= 60;
-
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
 
     document.getElementById('days').innerHTML = d;
     document.getElementById('hours').innerHTML = h;
     document.getElementById('minutes').innerHTML = m;
     document.getElementById('seconds').innerHTML = s;
-}, 1000);
+    document.getElementById('title').innerHTML = "Countdown to the " + eventDate.toLocaleDateString('en-US', options);
+}
+
+setInterval(countDown, 1000);
